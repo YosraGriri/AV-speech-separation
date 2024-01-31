6b23445bfc2cea55b23581fe3a44614563ca6e6c
YosraGriri <yosra.griri@gmail.com>
2024-02-01 00:18:48 +0100

    Fixed Looping over data and saving RIR

--- Scripts/main.py
+++ Scripts/main.py
@@ -23,96 +23,103 @@ materials = pra.Material(e_absorption)
 print(e_absorption, max_order)
 max_order = max_order
 print('-----------------------------------------------------------------------------')
+# Specify the source directory
+source_directory = "C:/Users/yosra/Documents/AV-speech-separation/Datasets/Grid Corpus/audio_25k/"
+# Output folder for saving simulated RIRs
+output_folder = "../Datasets/Grid Corpus/simulated_RIR"
 
-# specify signal source
-# Iteration over all files
-source_directory = "C:/Users/yosra/Documents/AV-speech-separation/Datasets/Grid Corpus/audio_25k/s1/"
-for filename in os.listdir(source_directory):
-    if filename.endswith(".wav"):
-        wav_path = os.path.join(source_directory, filename)
-        print('-----------------------------------------------------------------------------')
+# Iterate over subfolders in the source directory
+for subfolder in os.listdir(source_directory):
+    subfolder_path = os.path.join(source_directory, subfolder)
 
-        print(wav_path)
-        print('-----------------------------------------------------------------------------')
+    # Check if the item in the directory is a subfolder
+    if os.path.isdir(subfolder_path):
+        print(f"Processing subfolder: {subfolder}")
 
-        fs, signal = wavfile.read(wav_path)
+        # Iterate over audio files in the subfolder
+        for filename in os.listdir(subfolder_path):
+            if filename.endswith(".wav"):
+                wav_path = os.path.join(subfolder_path, filename)
+                print(f"Processing audio file: {wav_path}")
+                print('-----------------------------------------------------------------------------')
 
-        print(fs)
-        print(signal.shape)
-        ### Source variables
-        # Height of a person (170 cm)
-        person_height = 1.7
-        center_x, center_y = room_dimensions[0] / 2, room_dimensions[1] / 2
-        glasses_width = 0.139
-        current_directory = os.getcwd()
-        project_directory = os.path.abspath(os.path.join(current_directory, "../"))
+                print(wav_path)
+                print('-----------------------------------------------------------------------------')
+                fs, signal = wavfile.read(wav_path)
+                ### Source variables
+                # Height of a person (170 cm)
+                person_height = 1.7
+                center_x, center_y = room_dimensions[0] / 2, room_dimensions[1] / 2
+                glasses_width = 0.139
+                current_directory = os.getcwd()
+                project_directory = os.path.abspath(os.path.join(current_directory, "../"))
 
-        # Speaker information
-        speaker_position = [center_x + math.sqrt(2) / 2,
-                            center_y - math.sqrt(2) / 2,
-                            person_height]
-        print(f"Speaker Position: X = {speaker_position[0]}, "
-              f"Y = {speaker_position[1]},"
-              f" Z = {speaker_position[2]}")
+                # Speaker information
+                speaker_position = [center_x + math.sqrt(2) / 2,
+                                    center_y - math.sqrt(2) / 2,
+                                    person_height]
+                print(f"Speaker Position: X = {speaker_position[0]}, "
+                      f"Y = {speaker_position[1]},"
+                      f" Z = {speaker_position[2]}")
 
-        # Distance from the center to each microphone (assume glasses width of 20 cm)
-        mic_array = np.array([
+                # Distance from the center to each microphone (assume glasses width of 20 cm)
+                mic_array = np.array([
 
-            [center_x + glasses_width / 2,
-             center_y + glasses_width / 2,
-             person_height],
+                    [center_x + glasses_width / 2,
+                     center_y + glasses_width / 2,
+                     person_height],
 
-            [center_x + glasses_width / 2,
-             center_y - glasses_width / 2,
-             person_height],
+                    [center_x + glasses_width / 2,
+                     center_y - glasses_width / 2,
+                     person_height],
 
-            [center_x - glasses_width / 2,
-             center_y + glasses_width / 2,
-             person_height],
+                    [center_x - glasses_width / 2,
+                     center_y + glasses_width / 2,
+                     person_height],
 
-            [center_x - glasses_width / 2,
-             center_y - glasses_width / 2,
-             person_height]
-        ])
+                    [center_x - glasses_width / 2,
+                     center_y - glasses_width / 2,
+                     person_height]
+                ])
 
-        shoebox = create_room(fs,
-                              shoebox=True,
-                              room_dimensions=room_dimensions,
-                              room_corners=None,
-                              absorption_coefficient=absorption_coefficient,
-                              max_order=max_order,
-                              materials=materials,
-                              ray_tracing=True,
-                              air_absorption=True)
+                shoebox = create_room(fs,
+                                      shoebox=True,
+                                      room_dimensions=room_dimensions,
+                                      room_corners=None,
+                                      absorption_coefficient=absorption_coefficient,
+                                      max_order=max_order,
+                                      materials=materials,
+                                      ray_tracing=True,
+                                      air_absorption=True)
 
-        # Set the ray tracing parameters
-        shoebox.set_ray_tracing(receiver_radius=receiver_radius,
-                                n_rays=n_rays,
-                                energy_thres=energy_thres)
+                # Set the ray tracing parameters
+                shoebox.set_ray_tracing(receiver_radius=receiver_radius,
+                                        n_rays=n_rays,
+                                        energy_thres=energy_thres)
 
-        # Add microphone array
-        add_microphone_array(shoebox,
-                             mic_array
-                             )
+                # Add microphone array
+                add_microphone_array(shoebox,
+                                     mic_array
+                                     )
 
-        # Add source
+                # Add source
 
-        add_sound_source(shoebox, speaker_position, signal)
-        shoebox.image_source_model()
+                add_sound_source(shoebox, speaker_position, signal)
+                shoebox.image_source_model()
 
-        plot_room(shoebox,
-                  source_coordinates=speaker_position,
-                  mic_coordinates=mic_array,
-                  xlim=room_dimensions[0],
-                  ylim=room_dimensions[1],
-                  zlim=room_dimensions[2],
-                  save_path=project_directory)
-        shoebox.plot_rir()
-        # Compute RIR
-        shoebox.compute_rir()
-        shoebox.simulate()
+                plot_room(shoebox,
+                          source_coordinates=speaker_position,
+                          mic_coordinates=mic_array,
+                          xlim=room_dimensions[0],
+                          ylim=room_dimensions[1],
+                          zlim=room_dimensions[2],
+                          save_path=project_directory)
 
+                shoebox.plot_rir()
+                shoebox.compute_rir()
+                shoebox.simulate()
 
-        # Save processed audio with RIR
-        output_folder = "../Datasets/Grid Corpus/simulated_RIR"
-        save_processed_audio(shoebox, wav_path, output_folder)
+                # Save processed audio with RIR in su bfolder
+                subfolder_output = os.path.join(output_folder, subfolder)
+                save_processed_audio(shoebox, wav_path, subfolder_output)
+
