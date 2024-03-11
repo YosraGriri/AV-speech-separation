2a5027b1d484768f6e290d844745586eff168892
YosraGriri <yosra.griri@gmail.com>
2024-03-11 14:59:36 +0100

    Update get_voice

--- Scripts/get_voice.py
+++ Scripts/get_voice.py
@@ -1,77 +1,64 @@
 import os
 import random
 import librosa
+from pathlib import Path
+
+
+# Function to recursively find a folder with wav files
+def find_wav_files(folder):
+    """
+    Navigates through a nested directory structure to find .wav audio files.
+
+    Starting from the specified 'folder', it randomly selects subdirectories
+    to continue the search if subdirectories exist. This process is repeated
+    until a directory without further subdirectories is reached. At this point,
+    it searches for and returns a list of .wav files found within. If no .wav
+    files are present in the final directory, an empty list is returned.
+    Parameters:
+    - folder (str): The starting directory path for the search.
+    Returns:
+    - list: A list of paths to .wav files found in the directory without
+            further subdirectories. Returns an empty list if no .wav files
+            are found.
+    """
+    while True:
+        subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
+        if not subfolders:
+            # No subfolders, check for wav files in the current folder
+            wav_files = [f.path for f in os.scandir(folder) if f.is_file() and f.name.endswith('.wav')]
+            return wav_files
+        else:
+            # Randomly select a subfolder to dive into
+            folder = random.choice(subfolders)
 
 def get_voices(args):
     """
-    Randomly selects non-silent voices from the specified directory or its subfolders.
+    Randomly selects non-silent voices from the specified directory, delving into subfolders as needed.
 
     Parameters:
-    - args: Namespace, Command-line arguments or configuration parameters. Expected to have source_dir, sr, n_sources.
+    - args: Namespace, Command-line arguments or configuration parameters.
 
     Returns:
     - voices_data: list of tuples, Each tuple contains voice data (numpy array) and its identity.
-    - video_paths: list of str, Paths to video files found in the selected directories.
+    - video_paths: list of str, Paths to voice files found.
     """
-    # Check if there are subfolders in the source directory
-    subfolders = [f.path for f in os.scandir(args.source_dir) if f.is_dir()]
-    print('Checking for subfolders...')
-    print(f'---------------------------------------------\n'
-          f'{subfolders}\n'
-          f'---------------------------------------------')
-
-    selected_folders = []
-    if len(subfolders) < 2:
-        print('No subfolders found, using the main directory.')
-        # No subfolders, use the main directory
-        selected_folders = [args.source_dir]
-    else:
-        # Select random subfolders
-        selected_folders = random.sample(subfolders, min(len(subfolders), args.n_sources))
-
     voices_data = []
     video_paths = []
+    selected_wav_files = find_wav_files(args.source_dir)
 
-    for folder in selected_folders:
-        # Get a list of wav files in the folder
-        wav_files = [f.path for f in os.scandir(folder) if f.is_file() and f.name.endswith('.wav')]
+    for selected_wav_file in selected_wav_files:
+        if selected_wav_file not in video_paths:
+            video_paths.append(selected_wav_file)
 
-        # Skip folders with no WAV files
-        if not wav_files:
+        voice_identity = Path(selected_wav_file).stem.split("_")[0]
+        voice, _ = librosa.load(selected_wav_file, sr=args.sr, mono=True)
+        voice, _ = librosa.effects.trim(voice)
+
+        if voice.std() == 0:  # Skip silent voices
             continue
 
-        selected_wav_files = []
-        # Here, select two random wav files regardless of the number of subfolders if there are enough wav files
-        if len(wav_files) > 1:
-            selected_wav_files = random.sample(wav_files, 2)  # Ensure there are at least two to choose from
-        else:
-            selected_wav_files = wav_files  # Use whatever is available if less than 2
+        voices_data.append((voice, voice_identity))
 
-        for selected_wav_file in selected_wav_files:
-            print(f'Selected file: {selected_wav_file}')
-            # Extract voice identity from the filename
-            voice_identity = os.path.basename(selected_wav_file).split("_")[0]
-
-            # Load and process the voice data
-            voice, _ = librosa.load(selected_wav_file, sr=args.sr, mono=True)
-            voice, _ = librosa.effects.trim(voice)
-
-            # Skip silent voices
-            if voice.std() == 0:
-                continue
-
-            # Append the voice data and identity to the list
-            voices_data.append((voice, voice_identity))
-
-            # Break if the desired number of voices is reached
-            if len(voices_data) >= args.n_sources:
-                break
-
-        # This section remains unchanged - it collects video file paths
-        video_files = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith('.mp4')]
-        video_paths.extend(video_files)
-
-        # If we have collected enough voices, no need to continue
         if len(voices_data) >= args.n_sources:
             break
 
