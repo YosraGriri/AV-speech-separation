69777136d16d823736dd1311f874863faa5a9548
YosraGriri <yosra.griri@gmail.com>
2024-07-07 20:43:41 +0200

    Deleted unnecessary files and modified specific files

--- Simulator/Scripts/test_duration_calculation.py
+++
@@ -1,7 +0,0 @@
-import librosa
-
-# Replace 'path_to_audio_file.wav' with the actual path to one of your audio files
-audio_path = "../../data/VoxCeleb2/raw_audio_test/id04030/7mXUMuo5_NE/00001.wav"
-audio, sr = librosa.load(audio_path, sr=16000)  # Load with original sample rate
-duration = len(audio) / sr
-print(f"Duration: {duration} seconds")
