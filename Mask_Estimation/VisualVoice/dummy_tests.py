69db849a2d92e377f8b0edd91c80de1d4bde2bb4
YosraGriri <yosra.griri@gmail.com>
2024-07-08 16:13:27 +0200

    deleted unncessary old files

--- Mask_Estimation/VisualVoice/dummy_tests.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from pprint import pprint
-import argparse
-import librosa
-from utils import utils
-gev = np.load('jupyter notebooks/gev.np.npy')
-utils.istft_reconstruction(gev,)
